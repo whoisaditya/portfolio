6c6d85abf557152afe7bbd147183d0964b3b1399
whoisaditya <mitraaditya1357@gmail.com>
2023-07-29 12:00:40 +0530

    feat: added file structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio
-A portfolio website made using Reactjs to showcase my projects and other achievments 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
