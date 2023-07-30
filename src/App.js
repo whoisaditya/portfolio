4a4ebefb663cb7cd61e63aba778ea5304b6854fa
whoisaditya <mitraaditya1357@gmail.com>
2023-07-31 00:05:01 +0530

    fix: main width

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="text-gray-400 bg-gray-900 body-font lg:w-auto">
       <Navbar />
       <About />
       <Projects /> 
