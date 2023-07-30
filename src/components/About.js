e6edc0e14346963ee40332a00d4b06969075e658
whoisaditya <mitraaditya1357@gmail.com>
2023-07-31 00:13:35 +0530

    fix: for mobile screens

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,24 @@
+import React from "react";
+
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Aditya.
-            <br className="hidden lg:inline-block " />{" "}
+            Hi, I'm Aditya. <br className="hidden lg:inline-block" />
             <div className="animate-typing overflow-hidden whitespace-nowrap lg:w-fit">
               This Ken is a Backend Developer. &nbsp;&nbsp;
             </div>
           </h1>
-          <p className="mb-8 leading-relaxed lg:w-fit">
+          <p className="mb-8 leading-relaxed text-base lg:text-lg">
             I'm a motivated Computer Science and Engineering student that loves
             backend development and wants to use technology for good. I have an
-            8.55 CGPA and am pursuing a <a className="text-white">Bachelor of Technology in Computer
-            Science and Engineering</a> at the{" "}
+            8.55 CGPA and am pursuing a{" "}
+            <a className="text-white" href="https://vit.ac.in/">
+              Bachelor of Technology in Computer Science and Engineering
+            </a>{" "}
+            at the{" "}
             <a className="text-white" href="https://vit.ac.in/">
               Vellore Institute of Technology, Vellore.
             </a>{" "}
